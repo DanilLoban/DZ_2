fe31977eb40310be86c48f189b377bce47fc147f
Danil <dan.loban@yandex.ru>
2022-12-06 19:52:41 +0300

    first

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите трехзначное число, ");
+int N = Convert.ToInt32(Console.ReadLine());
+int S = (N / 10) % 10;
+Console.Write(S);
