9fb789eb3d580520f28d4fa500ce17785233b6f4
Danil <dan.loban@yandex.ru>
2022-12-09 00:30:46 +0300

    second

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите трехзначное число, ");
+int N = Convert.ToInt32(Console.ReadLine());
+if(N < 100)
+{
+    Console.Write("нет 3 числа");
+}
+else
+{
+while (N >= 1000)
+{
+    N = N / 10;
+}
+Console.Write(N % 10);
+}
