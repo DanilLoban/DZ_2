a2d8362c00dc911789b5b3c785b0d45be32a1449
Danil <dan.loban@yandex.ru>
2022-12-09 00:40:16 +0300

    third

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число, ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 23 == 0)
+{
+    if (N % 7 == 0)
+    {
+        Console.Write("Да");
+    }
+    else
+    {
+         Console.Write("Нет");
+    }
+}
+else 
+{
+    Console.Write("Нет");
+}
